9bbf1a2432bc3b1a85013acc0c171a599457fff5
Anwar Zahid <anwarzahid.bcs@gmail.com>
2022-06-23 11:34:58 -0700

    Dagger-Hilt Dependency injection

--- app/src/main/java/com/example/dagger_hilt_di/TestViewModel.kt
+++ app/src/main/java/com/example/dagger_hilt_di/TestViewModel.kt
@@ -1,14 +1,9 @@
 package com.example.dagger_hilt_di
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
-import javax.inject.Inject
-import javax.inject.Named
 
 @HiltViewModel
-class TestViewModel @Inject  constructor( @Named("activityMString")val activityString:String): ViewModel() {
-    init {
-        Log.d("viewModelString", "$activityString: ")
-    }
+class TestViewModel : ViewModel() {
+
 }
